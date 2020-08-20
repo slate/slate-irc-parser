96a80d317c0e27354940899a8b13c7be24e8b112
Hyeon Kim <simnalamburt@gmail.com>
2020-08-21 03:42:26 +0900

    Rewrite using ES2015 class syntax

--- index.ts
+++ index.ts
@@ -5,91 +5,84 @@ import { Stream, Duplex } from 'stream'
 
 const debug = debugModule('slate-irc-parser')
 
-interface Parser extends Stream {
+export default class Parser extends Stream {
   writable: boolean
   nlstream: Duplex
 
-  write(chunk: Buffer): boolean
-  online(line: String): void
-}
-
-/**
- * Initialize IRC parser.
- *
- * @api public
- */
-export default function Parser(this: Parser) {
-  this.writable = true
-  this.nlstream = linewise.getPerLineBuffer()
-  this.nlstream.on('data', this.online.bind(this))
-  this.nlstream.resume()
-}
-
-/**
- * Inherit from `Stream.prototype`.
- */
-Parser.prototype.__proto__ = Stream.prototype
-
-/**
- * Write `chunk`.
- *
- * @param {Buffer} chunk
- * @api public
- */
-Parser.prototype.write = function (this: Parser, chunk: Buffer): boolean {
-  return this.nlstream.write(chunk)
-}
-
-/**
- * Parse lines and emit "message" events.
- *
- * @param {String} line
- * @api private
- */
-Parser.prototype.online = function (this: Parser, line: String): void {
-  // Remove a single CR at the end of the line if it does exist
-  line = line.replace(/\r$/, '')
-
-  debug('line %s', util.inspect(line))
-  const orig = line
-
-  // prefix
-  let prefix
-  if (':' == line[0]) {
-    const i = line.indexOf(' ')
-    prefix = line.slice(1, i)
-    line = line.slice(i + 1)
+  /**
+   * Initialize IRC parser.
+   *
+   * @api public
+   */
+  constructor() {
+    super()
+    this.writable = true
+    this.nlstream = linewise.getPerLineBuffer()
+    this.nlstream.on('data', this.online.bind(this))
+    this.nlstream.resume()
   }
 
-  // command
-  let i = line.indexOf(' ')
-  if (-1 == i) i = line.length
-  const command = line.slice(0, i)
-  line = line.slice(i)
-
-  // params
-  i = line.indexOf(' :')
-  if (-1 == i) i = line.length
-  const params = line.slice(1, i)
-  line = line.slice(i + 2)
-
-  const msg = {
-    prefix: prefix || '',
-    command: command,
-    params: params || '',
-    trailing: line || '',
-    string: orig,
+  /**
+   * Write `chunk`.
+   *
+   * @param {Buffer} chunk
+   * @api public
+   */
+  write(chunk: Buffer): boolean {
+    return this.nlstream.write(chunk)
   }
 
-  debug('message %j', msg)
-  this.emit('message', msg)
-}
+  /**
+   * Parse lines and emit "message" events.
+   *
+   * @param {String} line
+   * @api private
+   */
+  online(line: String): void {
+    // Remove a single CR at the end of the line if it does exist
+    line = line.replace(/\r$/, '')
 
-/**
- * Emit "end".
- *
- * @api public
- */
-Parser.prototype.end = function () {
-  this.emit('end')
+    debug('line %s', util.inspect(line))
+    const orig = line
+
+    // prefix
+    let prefix
+    if (':' == line[0]) {
+      const i = line.indexOf(' ')
+      prefix = line.slice(1, i)
+      line = line.slice(i + 1)
+    }
+
+    // command
+    let i = line.indexOf(' ')
+    if (-1 == i) i = line.length
+    const command = line.slice(0, i)
+    line = line.slice(i)
+
+    // params
+    i = line.indexOf(' :')
+    if (-1 == i) i = line.length
+    const params = line.slice(1, i)
+    line = line.slice(i + 2)
+
+    const msg = {
+      prefix: prefix || '',
+      command: command,
+      params: params || '',
+      trailing: line || '',
+      string: orig,
+    }
+
+    debug('message %j', msg)
+    this.emit('message', msg)
+  }
+
+  /**
+   * Emit "end".
+   *
+   * @api public
+   */
+  end() {
+    this.emit('end')
+  }
 }
